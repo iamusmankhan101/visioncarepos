80336f552f47fdea6f7abdbbb3e49bcfa82ba1c3
Usman Khan <imusmankhan10@gmail.com>
2025-12-28 15:42:06 +0500

    Fix prescription display in invoice - use contact_id

--- resources/views/sale_pos/receipts/classic.blade.php
+++ resources/views/sale_pos/receipts/classic.blade.php
@@ -272,64 +272,67 @@
 </div>
 
 {{-- Prescription Table --}}
-@if(!empty($receipt_details->contact))
-	@php
-		$contact = \App\Contact::find($receipt_details->contact->id ?? null);
-	@endphp
-	@if($contact && ($contact->custom_field1 || $contact->custom_field2 || $contact->custom_field3 || $contact->custom_field4 || $contact->custom_field5 || $contact->custom_field6 || $contact->custom_field7 || $contact->custom_field8 || $contact->custom_field9 || $contact->custom_field10))
-		<div class="row" style="color: #000000 !important; margin-top: 10px;">
-			<div class="col-xs-12">
-				<h4 style="margin: 10px 0; color: #48b2ee;"><i class="fa fa-eye"></i> Prescription Details</h4>
-				<table class="table table-bordered table-condensed" style="margin-bottom: 10px;">
-					<thead style="background-color: #48b2ee; color: white;">
-						<tr>
-							<th style="width: 15%;">Eye</th>
-							<th style="width: 15%;">Type</th>
-							<th style="width: 23%;">Sph.</th>
-							<th style="width: 23%;">Cyl.</th>
-							<th style="width: 24%;">Axis</th>
-						</tr>
-					</thead>
-					<tbody>
-						<!-- RIGHT EYE - Distance -->
-						<tr>
-							<td rowspan="2" style="vertical-align: middle; font-weight: bold; background-color: #f8f9fa;">
-								<i class="fa fa-arrow-right" style="color: #48b2ee;"></i> RIGHT EYE
-							</td>
-							<td style="font-weight: 600;">Distance</td>
-							<td>{{ $contact->custom_field1 ?? '-' }}</td>
-							<td>{{ $contact->custom_field2 ?? '-' }}</td>
-							<td>{{ $contact->custom_field3 ?? '-' }}</td>
-						</tr>
-						<!-- RIGHT EYE - Near -->
-						<tr>
-							<td style="font-weight: 600;">Near</td>
-							<td>{{ $contact->custom_field4 ?? '-' }}</td>
-							<td>{{ $contact->custom_field5 ?? '-' }}</td>
-							<td>{{ $contact->custom_field6 ?? '-' }}</td>
-						</tr>
-						<!-- LEFT EYE - Distance -->
-						<tr>
-							<td rowspan="2" style="vertical-align: middle; font-weight: bold; background-color: #f8f9fa;">
-								<i class="fa fa-arrow-left" style="color: #48b2ee;"></i> LEFT EYE
-							</td>
-							<td style="font-weight: 600;">Distance</td>
-							<td>{{ $contact->custom_field7 ?? '-' }}</td>
-							<td>{{ $contact->custom_field8 ?? '-' }}</td>
-							<td>{{ $contact->custom_field9 ?? '-' }}</td>
-						</tr>
-						<!-- LEFT EYE - Near -->
-						<tr>
-							<td style="font-weight: 600;">Near</td>
-							<td>{{ $contact->custom_field10 ?? '-' }}</td>
-							<td>{{ !empty($contact->shipping_custom_field_details['shipping_custom_field_1']) ? $contact->shipping_custom_field_details['shipping_custom_field_1'] : '-' }}</td>
-							<td>{{ !empty($contact->shipping_custom_field_details['shipping_custom_field_2']) ? $contact->shipping_custom_field_details['shipping_custom_field_2'] : '-' }}</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+@php
+	$contact_id = $receipt_details->contact_id ?? null;
+	if($contact_id) {
+		$contact = \App\Contact::find($contact_id);
+	} else {
+		$contact = null;
+	}
+@endphp
+@if($contact && ($contact->custom_field1 || $contact->custom_field2 || $contact->custom_field3 || $contact->custom_field4 || $contact->custom_field5 || $contact->custom_field6 || $contact->custom_field7 || $contact->custom_field8 || $contact->custom_field9 || $contact->custom_field10))
+	<div class="row" style="color: #000000 !important; margin-top: 10px;">
+		<div class="col-xs-12">
+			<h4 style="margin: 10px 0; color: #48b2ee;"><i class="fa fa-eye"></i> Prescription Details</h4>
+			<table class="table table-bordered table-condensed" style="margin-bottom: 10px;">
+				<thead style="background-color: #48b2ee; color: white;">
+					<tr>
+						<th style="width: 15%;">Eye</th>
+						<th style="width: 15%;">Type</th>
+						<th style="width: 23%;">Sph.</th>
+						<th style="width: 23%;">Cyl.</th>
+						<th style="width: 24%;">Axis</th>
+					</tr>
+				</thead>
+				<tbody>
+					<!-- RIGHT EYE - Distance -->
+					<tr>
+						<td rowspan="2" style="vertical-align: middle; font-weight: bold; background-color: #f8f9fa;">
+							<i class="fa fa-arrow-right" style="color: #48b2ee;"></i> RIGHT EYE
+						</td>
+						<td style="font-weight: 600;">Distance</td>
+						<td>{{ $contact->custom_field1 ?? '-' }}</td>
+						<td>{{ $contact->custom_field2 ?? '-' }}</td>
+						<td>{{ $contact->custom_field3 ?? '-' }}</td>
+					</tr>
+					<!-- RIGHT EYE - Near -->
+					<tr>
+						<td style="font-weight: 600;">Near</td>
+						<td>{{ $contact->custom_field4 ?? '-' }}</td>
+						<td>{{ $contact->custom_field5 ?? '-' }}</td>
+						<td>{{ $contact->custom_field6 ?? '-' }}</td>
+					</tr>
+					<!-- LEFT EYE - Distance -->
+					<tr>
+						<td rowspan="2" style="vertical-align: middle; font-weight: bold; background-color: #f8f9fa;">
+							<i class="fa fa-arrow-left" style="color: #48b2ee;"></i> LEFT EYE
+						</td>
+						<td style="font-weight: 600;">Distance</td>
+						<td>{{ $contact->custom_field7 ?? '-' }}</td>
+						<td>{{ $contact->custom_field8 ?? '-' }}</td>
+						<td>{{ $contact->custom_field9 ?? '-' }}</td>
+					</tr>
+					<!-- LEFT EYE - Near -->
+					<tr>
+						<td style="font-weight: 600;">Near</td>
+						<td>{{ $contact->custom_field10 ?? '-' }}</td>
+						<td>{{ !empty($contact->shipping_custom_field_details['shipping_custom_field_1']) ? $contact->shipping_custom_field_details['shipping_custom_field_1'] : '-' }}</td>
+						<td>{{ !empty($contact->shipping_custom_field_details['shipping_custom_field_2']) ? $contact->shipping_custom_field_details['shipping_custom_field_2'] : '-' }}</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
-	@endif
+	</div>
 @endif
 
 <div class="row" style="color: #000000 !important;">
