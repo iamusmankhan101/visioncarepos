e218608b68b5ef7178e50a209e15f45017ec58f3
Usman Khan <imusmankhan10@gmail.com>
2026-01-26 21:30:16 +0500

    n

---
+++ fix_icheck_visibility_issue.js
@@ -0,0 +1,8 @@
+/**
+ * Fix for iCheck Visibility Issue
+ * 
+ * This addresses the specific case where iCheck initializes (helper element is present)
+ * but the actual checkbox is not visible due to CSS or positioning issues.
+ */
+
+(
