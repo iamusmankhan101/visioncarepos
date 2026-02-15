052905fa5777564c0a6ad21bfab091a5498d85ce
Usman Khan <imusmankhan10@gmail.com>
2026-02-15 14:00:38 +0500

    kj

--- resources/views/sale_pos/create.blade.php
+++ resources/views/sale_pos/create.blade.php
@@ -469,32 +469,35 @@
     });
     // Pending Shimpmets
     $(document).ready(function(){
-        var pending_shipments_table = $('#pending_shipments_table').DataTable({
-            processing: true,
-            serverSide: true,
-            aaSorting: [[1, 'desc']],
-            "ajax": {
-                "url": "/sells",
-                "data": function ( d ) {
-                    d.only_shipments = 'true';
-                    if($('#location_id').length) {
-                        d.location_id = $('#location_id').val();
+        if ($('#pending_shipments_table').length) {
+            var pending_shipments_table = $('#pending_shipments_table').DataTable({
+                processing: true,
+                serverSide: true,
+                destroy: true,
+                aaSorting: [[1, 'desc']],
+                "ajax": {
+                    "url": "/sells",
+                    "data": function ( d ) {
+                        d.only_shipments = 'true';
+                        if($('#location_id').length) {
+                            d.location_id = $('#location_id').val();
+                        }
                     }
-                }
-            },
-            columnDefs: [ {
-                "targets": 4,
-                "orderable": false,
-                "searchable": false
-            } ],
-            columns: [
-                { data: 'action', name: 'action', searchable: false, orderable: false},
-                { data: 'transaction_date', name: 'transaction_date'  },
-                { data: 'invoice_no', name: 'invoice_no'},
-                { data: 'contact_name', name: 'contact_name'},
-                { data: 'shipping_status', name: 'shipping_status'}
-            ]
-        });
+                },
+                columnDefs: [ {
+                    "targets": 4,
+                    "orderable": false,
+                    "searchable": false
+                } ],
+                columns: [
+                    { data: 'action', name: 'action', searchable: false, orderable: false},
+                    { data: 'transaction_date', name: 'transaction_date'  },
+                    { data: 'invoice_no', name: 'invoice_no'},
+                    { data: 'contact_name', name: 'contact_name'},
+                    { data: 'shipping_status', name: 'shipping_status'}
+                ]
+            });
+        }
     });
     </script>
 @endsection
