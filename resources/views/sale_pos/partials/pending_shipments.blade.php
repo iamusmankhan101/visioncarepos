770320e7dd8bdbabf3100859c575ef1488469edd
Usman Khan <imusmankhan10@gmail.com>
2026-02-15 13:55:02 +0500

    nm

--- resources/views/sale_pos/partials/pending_shipments.blade.php
+++ resources/views/sale_pos/partials/pending_shipments.blade.php
@@ -19,33 +19,4 @@
     </div>
 </div>
 
-<script type="text/javascript">
-$(document).ready(function(){
-    var pending_shipments_table = $('#pending_shipments_table').DataTable({
-        processing: true,
-        serverSide: true,
-        aaSorting: [[1, 'desc']],
-        "ajax": {
-            "url": "/sells",
-            "data": function ( d ) {
-                d.only_shipments = 'true';
-                if($('#location_id').length) {
-                    d.location_id = $('#location_id').val();
-                }
-            }
-        },
-        columnDefs: [ {
-            "targets": 4,
-            "orderable": false,
-            "searchable": false
-        } ],
-        columns: [
-            { data: 'action', name: 'action', searchable: false, orderable: false},
-            { data: 'transaction_date', name: 'transaction_date'  },
-            { data: 'invoice_no', name: 'invoice_no'},
-            { data: 'contact_name', name: 'contact_name'},
-            { data: 'shipping_status', name: 'shipping_status'}
-        ]
-    });
-});
-</script>
+
