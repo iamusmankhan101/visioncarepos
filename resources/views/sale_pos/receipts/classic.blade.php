c3b48c905effd09671677b9f0b8559f9ae7dc127
Usman Khan <imusmankhan10@gmail.com>
2026-03-26 21:23:25 +0500

    fg

--- resources/views/sale_pos/receipts/classic.blade.php
+++ resources/views/sale_pos/receipts/classic.blade.php
@@ -384,9 +384,10 @@ window.addEventListener('afterprint', function() {
 		<table style="width: 100%; margin-bottom: 10px; border-collapse: collapse;">
 			<thead>
 				<tr style="border-bottom: 1px solid #eee;">
-					<th style="padding: 4px 0; text-align: left; width: 60%; font-size: 13px; color: #000;">Product</th>
-					<th style="padding: 4px 0; text-align: left; width: 20%; font-size: 13px; color: #000;">Quantity</th>
-					<th style="padding: 4px 0; text-align: left; width: 20%; font-size: 13px; color: #000;">Unit Price</th>
+					<th style="padding: 4px 0; text-align: left; width: 50%; font-size: 13px; color: #000;">Product</th>
+					<th style="padding: 4px 0; text-align: right; width: 17%; font-size: 13px; color: #000;">Quantity</th>
+					<th style="padding: 4px 0; text-align: right; width: 17%; font-size: 13px; color: #000;">Unit Price</th>
+					<th style="padding: 4px 0; text-align: right; width: 16%; font-size: 13px; color: #000;">Subtotal</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -396,11 +397,14 @@ window.addEventListener('afterprint', function() {
 						<td style="padding: 4px 0; font-size: 13px; color: #000;">
 							{{$line['name']}} {{$line['product_variation']}} {{$line['variation']}}
 						</td>
-						<td style="padding: 4px 0; font-size: 13px; color: #000;">
+						<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
 							{{$line['quantity']}} {{$line['units']}}
 						</td>
-						<td style="padding: 4px 0; font-size: 13px; color: #000;">
+						<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
 							{{$line['unit_price_inc_tax']}}
+						</td>
+						<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
+							{{$line['line_total']}}
 						</td>
 					</tr>
 					@endif
@@ -488,9 +492,10 @@ window.addEventListener('afterprint', function() {
 				<table style="width: 100%; margin-bottom: 10px; border-collapse: collapse;">
 					<thead>
 						<tr style="border-bottom: 1px solid #eee;">
-							<th style="padding: 4px 0; text-align: left; width: 60%; font-size: 13px; color: #000;">Product</th>
-							<th style="padding: 4px 0; text-align: left; width: 20%; font-size: 13px; color: #000;">Quantity</th>
-							<th style="padding: 4px 0; text-align: left; width: 20%; font-size: 13px; color: #000;">Unit Price</th>
+							<th style="padding: 4px 0; text-align: left; width: 50%; font-size: 13px; color: #000;">Product</th>
+							<th style="padding: 4px 0; text-align: right; width: 17%; font-size: 13px; color: #000;">Quantity</th>
+							<th style="padding: 4px 0; text-align: right; width: 17%; font-size: 13px; color: #000;">Unit Price</th>
+							<th style="padding: 4px 0; text-align: right; width: 16%; font-size: 13px; color: #000;">Subtotal</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -500,11 +505,14 @@ window.addEventListener('afterprint', function() {
 								<td style="padding: 4px 0; font-size: 13px; color: #000;">
 									{{$line['name']}} {{$line['product_variation']}} {{$line['variation']}}
 								</td>
-								<td style="padding: 4px 0; font-size: 13px; color: #000;">
+								<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
 									{{$line['quantity']}} {{$line['units']}}
 								</td>
-								<td style="padding: 4px 0; font-size: 13px; color: #000;">
+								<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
 									{{$line['unit_price_inc_tax']}}
+								</td>
+								<td style="padding: 4px 0; font-size: 13px; color: #000; text-align: right;">
+									{{$line['line_total']}}
 								</td>
 							</tr>
 							@endif
@@ -580,130 +588,6 @@ window.addEventListener('afterprint', function() {
 @endif
 
 <div class="row" style="color: #000000 !important;">
-	<div class="col-xs-12">
-		<br/>
-		@php
-			$p_width = 45;
-		@endphp
-		@if(!empty($receipt_details->item_discount_label))
-			@php
-				$p_width -= 10;
-			@endphp
-		@endif
-		@if(!empty($receipt_details->discounted_unit_price_label))
-			@php
-				$p_width -= 10;
-			@endphp
-		@endif
-		<table class="table table-responsive table-slim">
-			<thead>
-				<tr>
-					<th width="{{$p_width}}%">{{$receipt_details->table_product_label}}</th>
-					<th class="text-right" width="15%">{{$receipt_details->table_qty_label}}</th>
-					<th class="text-right" width="15%">{{$receipt_details->table_unit_price_label}}</th>
-					@if(!empty($receipt_details->discounted_unit_price_label))
-						<th class="text-right" width="10%">{{$receipt_details->discounted_unit_price_label}}</th>
-					@endif
-					@if(!empty($receipt_details->item_discount_label))
-						<th class="text-right" width="10%">{{$receipt_details->item_discount_label}}</th>
-					@endif
-					<th class="text-right" width="15%">{{$receipt_details->table_subtotal_label}}</th>
-				</tr>
-			</thead>
-			<tbody>
-				@forelse($receipt_details->lines as $line)
-					<tr>
-						<td>
-							@if(!empty($line['image']))
-								<img src="{{$line['image']}}" alt="Image" width="50" style="float: left; margin-right: 8px;">
-							@endif
-                            {{$line['name']}} {{$line['product_variation']}} {{$line['variation']}} 
-                            @if(!empty($line['sub_sku'])), {{$line['sub_sku']}} @endif @if(!empty($line['brand'])), {{$line['brand']}} @endif @if(!empty($line['cat_code'])), {{$line['cat_code']}}@endif
-                            @if(!empty($line['product_custom_fields'])), {{$line['product_custom_fields']}} @endif
-                            
-                            @if(!empty($line['product_description']))
-                            	<small>
-                            		{!!$line['product_description']!!}
-                            	</small>
-                            @endif 
-                            @if(!empty($line['sell_line_note']))
-                            <br>
-                            <small>
-                            	{!!$line['sell_line_note']!!}
-                            </small>
-                            @endif 
-                            @if(!empty($line['lot_number']))<br> {{$line['lot_number_label']}}:  {{$line['lot_number']}} @endif 
-                            @if(!empty($line['product_expiry'])), {{$line['product_expiry_label']}}:  {{$line['product_expiry']}} @endif
-
-                            @if(!empty($line['warranty_name'])) <br><small>{{$line['warranty_name']}} </small>@endif @if(!empty($line['warranty_exp_date'])) <small>- {{@format_date($line['warranty_exp_date'])}} </small>@endif
-                            @if(!empty($line['warranty_description'])) <small> {{$line['warranty_description'] ?? ''}}</small>@endif
-
-                            @if($receipt_details->show_base_unit_details && $line['quantity'] && $line['base_unit_multiplier'] !== 1)
-                            <br><small>
-                            	1 {{$line['units']}} = {{$line['base_unit_multiplier']}} {{$line['base_unit_name']}} <br>
-                            	{{$line['base_unit_price']}} x {{$line['orig_quantity']}} = {{$line['line_total']}}
-                            </small>
-                            @endif
-                        </td>
-						<td class="text-right">
-							{{$line['quantity']}} {{$line['units']}} 
-
-							@if($receipt_details->show_base_unit_details && $line['quantity'] && $line['base_unit_multiplier'] !== 1)
-                            <br><small>
-                            	{{$line['quantity']}} x {{$line['base_unit_multiplier']}} = {{$line['orig_quantity']}} {{$line['base_unit_name']}}
-                            </small>
-                            @endif
-						</td>
-						<td class="text-right">{{$line['unit_price_before_discount']}}</td>
-						@if(!empty($receipt_details->discounted_unit_price_label))
-							<td class="text-right">
-								{{$line['unit_price_inc_tax']}} 
-							</td>
-						@endif
-						@if(!empty($receipt_details->item_discount_label))
-							<td class="text-right">
-								{{$line['total_line_discount'] ?? '0.00'}}
-
-								@if(!empty($line['line_discount_percent']))
-								 	({{$line['line_discount_percent']}}%)
-								@endif
-							</td>
-						@endif
-						<td class="text-right">{{$line['line_total']}}</td>
-					</tr>
-					@if(!empty($line['modifiers']))
-						@foreach($line['modifiers'] as $modifier)
-							<tr>
-								<td>
-		                            {{$modifier['name']}} {{$modifier['variation']}} 
-		                            @if(!empty($modifier['sub_sku'])), {{$modifier['sub_sku']}} @endif @if(!empty($modifier['cat_code'])), {{$modifier['cat_code']}}@endif
-		                            @if(!empty($modifier['sell_line_note']))({!!$modifier['sell_line_note']!!}) @endif 
-		                        </td>
-								<td class="text-right">{{$modifier['quantity']}} {{$modifier['units']}} </td>
-								<td class="text-right">{{$modifier['unit_price_inc_tax']}}</td>
-								@if(!empty($receipt_details->discounted_unit_price_label))
-									<td class="text-right">{{$modifier['unit_price_exc_tax']}}</td>
-								@endif
-								@if(!empty($receipt_details->item_discount_label))
-									<td class="text-right">0.00</td>
-								@endif
-								<td class="text-right">{{$modifier['line_total']}}</td>
-							</tr>
-						@endforeach
-					@endif
-				@empty
-					<tr>
-						<td colspan="4">&nbsp;</td>
-						@if(!empty($receipt_details->discounted_unit_price_label))
-    					<td></td>
-    					@endif
-    					@if(!empty($receipt_details->item_discount_label))
-    					<td></td>
-    					@endif
-					</tr>
-				@endforelse
-			</tbody>
-		</table>
 	</div>
 </div>
 
